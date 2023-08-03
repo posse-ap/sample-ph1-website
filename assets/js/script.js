a8dd921dd3dbaad1a175956cbf6ee518d18efcaa
kohei23n <kohei.s.n@gmail.com>
2023-08-03 16:29:58 +0900

    week10まで

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,13 @@
-"use strict";
+// const btn = document.getElementById('btn');
 
-/**
- * ハンバーガーメニュー
- * @see https://developer.mozilla.org/ja/docs/Web/API/Element/classList
- * @see https://developer.mozilla.org/ja/docs/Web/API/Document/body
- *
- * @description ヘッダーのハンバーガーメニューをクリックしたらヘッダーとbodyにクラスを付与する
- */
-// ヘッダー・ボタンの要素を取得
-const header = document.getElementById("js-header");
-const button = document.getElementById("js-headerButton");
+// btn.onclick = function() {
+//   console.log('こんにちは');
+// }
 
-// ボタンをクリックした時の処理
-button.addEventListener("click", () => {
-  header.classList.toggle("is-open");
-  document.body.classList.toggle("is-fixed");
-});
+const btn = document.getElementById('btn');
+
+const hello = function() {
+  console.log('こんにちは');
+}
+
+btn.onclick = hello;
