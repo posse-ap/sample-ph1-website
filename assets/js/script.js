62e63150a98e486f53c1938ef1b9a2dea4ea8e0b
kohei23n <kohei.s.n@gmail.com>
2023-08-03 20:40:06 +0900

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,17 @@ button.addEventListener("click", () => {
   header.classList.toggle("is-open");
   // document.body.classList.toggle("is-fixed");
 });
+
+
+// メインビジュアルの要素を取得
+const mainVisual = document.getElementById("js-mainVisual");
+
+// スクロールした時の処理
+window.addEventListener("scroll", () => {
+  // [スクロールした分の高さ] が [メインビジュアルの高さ - ヘッダーの高さ] より大きい時
+  if (window.scrollY > mainVisual.clientHeight - header.clientHeight) {
+    header.classList.remove("is-transparent");
+  } else {
+    header.classList.add("is-transparent");
+  }
+});
