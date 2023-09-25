431984c0455e8e53301b7527f41a62faf01cc875
kohei23n <kohei.s.n@gmail.com>
2023-09-25 15:50:56 +0900

    JS追記

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,18 @@
 // ヘッダー・ボタンの要素を取得
 const header = document.getElementById("js-header");
 const button = document.getElementById("js-headerButton");
+const navLinks = document.querySelectorAll('.header-navLink');
 
 // ボタンをクリックした時の処理
 button.addEventListener("click", () => {
   header.classList.toggle("is-open");
-  // document.body.classList.toggle("is-fixed");
+});
+
+// メニューの各リンクをクリックしたときの処理
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    header.classList.remove('is-open');
+  });
 });
 
 /* ヘッダーの透過 */
