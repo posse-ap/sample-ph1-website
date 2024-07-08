aa1d40cd067d3c8b4b3fe84f753273e003c6f710
kohei23n <kohei.s.n@gmail.com>
2024-07-08 11:55:15 +0900

    updated js for week11

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
+// ボタンをクリックした時の処理
 const header = document.getElementById("js-header");
 const button = document.getElementById("js-headerButton");
 
-// ボタンをクリックした時の処理
 button.addEventListener("click", () => {
   header.classList.toggle("is-open");
+});
+
+// スクロールした時の処理
+const mainVisual = document.getElementById("js-mainVisual");
+
+window.addEventListener("scroll", () => {
+  // [スクロールした分の高さ] が [メインビジュアルの高さ - ヘッダーの高さ] より大きい時
+  if (window.scrollY > mainVisual.clientHeight - header.clientHeight) {
+    header.classList.remove("is-transparent");
+  } else {
+    header.classList.add("is-transparent");
+  }
 });
